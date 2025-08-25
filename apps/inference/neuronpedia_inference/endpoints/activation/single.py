b4176319a848ee0a6b1dd2bfb11c30c0dc02001f
Shayan Sadeghieh <shayan.sadeghieh@gmail.com>
2025-08-24 21:29:20 -0400

    made it pass gpt2 forward pass for single endpoint

--- apps/inference/neuronpedia_inference/endpoints/activation/single.py
+++ apps/inference/neuronpedia_inference/endpoints/activation/single.py
@@ -73,7 +73,7 @@ async def activation_single(
             prompt,
             prepend_bos=prepend_bos,
             truncate=False,
-        )[0]        
+        )     
 
         if len(tokens) > config.token_limit:
             logger.error(
