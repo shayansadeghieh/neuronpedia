e8a114411d5468966a88f185e6c27f386a025bbc
Johnny Lin <hijohnnylin@gmail.com>
2025-09-12 16:17:28 -0700

    fix embed redirectToSteer

--- apps/webapp/app/[modelId]/[layer]/[index]/page.tsx
+++ apps/webapp/app/[modelId]/[layer]/[index]/page.tsx
@@ -45,7 +45,7 @@ export default async function Page({
     const redirectToSteer = searchParams.redirectToSteer === 'true';
     if (redirectToSteer) {
       const strength = searchParams.strength ? (searchParams.strength as string) : 10;
-      const redirectUrl = `/${REPLACE_MODEL_ID_MAP_FOR_LW_TEMPORARY_REDIRECT[params.modelId as keyof typeof REPLACE_MODEL_ID_MAP_FOR_LW_TEMPORARY_REDIRECT]}/steer?source=${params.layer}&index=${params.index}&strength=${strength}`;
+      const redirectUrl = `/${REPLACE_MODEL_ID_MAP_FOR_LW_TEMPORARY_REDIRECT[params.modelId as keyof typeof REPLACE_MODEL_ID_MAP_FOR_LW_TEMPORARY_REDIRECT]}/steer?embed=true&source=${params.layer}&index=${params.index}&strength=${strength}`;
       redirect(redirectUrl);
     } else {
       const queryString = new URLSearchParams(searchParams as Record<string, string>).toString();
